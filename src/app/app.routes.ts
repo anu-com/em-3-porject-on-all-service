274588de03dba600a6b07b6828abf0f5ac682a6f
anu-com <anuragkumar2225@gmail.com>
2024-03-06 17:14:36 +0530

    first change

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+export const routes: Routes = [
+    {path:'',redirectTo:"",pathMatch:'full'},
+    {path:"",component:HomeComponent},
+    {path:"services",component:ServicesComponent},
+    {path:"contact",component:ContactComponent},
+    {path:"about",component:AboutComponent},
+    
+];
