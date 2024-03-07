92ca9f635db3c908d944d998301f7ec32e905ed2
anu-com <anuragkumar2225@gmail.com>
2024-03-07 15:30:12 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,RouterLink,HeaderComponent,FooterComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
     {path:'',redirectTo:"",pathMatch:'full'},
@@ -10,5 +12,6 @@ export const routes: Routes = [
     {path:"services",component:ServicesComponent},
     {path:"contact",component:ContactComponent},
     {path:"about",component:AboutComponent},
+    {path:"login",component:LoginComponent},
     
 ];
